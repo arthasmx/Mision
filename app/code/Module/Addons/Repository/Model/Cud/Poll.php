30ce4fd0d1fd21b3ead238b353e06571d80af893
ArthasMX <arthasmx@gmail.com>
2012-02-13 10:51:47 -0700

    Removes comment coding left in previous commit, Fixes 44

--- app/code/Module/Addons/Repository/Model/Cud/Poll.php
+++ app/code/Module/Addons/Repository/Model/Cud/Poll.php
@@ -3,11 +3,10 @@ require_once 'Module/Core/Repository/Model/Db/Actions.php';
 class Module_Addons_Repository_Model_Cud_Poll extends Module_Core_Repository_Model_Db_Actions{
 
   function poll($id=0,$vote=null){
-    /*
     if ( $this->is_multiple_database_modification_attempt('poll_votes','created',FALSE) ){
       return false;
     }
-    */
+
     $this->set_table('poll_votes');
     $data = array('poll_id'   => $id  
                   ,'ip'       => $_SERVER['REMOTE_ADDR']
